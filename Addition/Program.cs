afd18fa8be99a16f0f9208081009956f3e8ecb75
dasre21 <dasre.brahmanand@gmail.com>
2023-11-23 17:09:24 +0530

    Initial commit

---
+++ Addition/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    internal class Program
+    {
+        public void Addition()
+        {
+            int a, b, c;
+
+            a= Convert.ToInt32(Console.ReadLine());
+
+            b= Convert.ToInt32(Console.ReadLine());
+
+            c = a + b;
+
+            Console.WriteLine(c);
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Addition();
+
+            Console.ReadLine();
+        }
+    }
+}
